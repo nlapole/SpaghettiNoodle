6afd9b22f524560e8878b4936e1e7e9d4341eff7
nlapole <nick.lapole@gmail.com>
2022-01-09 21:54:16 -0600

    Player Movement WIP
    Input system
    Player movement script
    based on
    Complete C# Unity Game Developer 2D

---
+++ MeatBalls/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    //Code as of:
+    //Complete C# Unity Game Developer 2D
+    //Video 83.
+
+
+
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 10f;
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if(value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+    }
+}
